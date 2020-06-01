659ffb9f9c53d458240816c221dfb8d6096a81e7
Isaiah Kelly <isaiahpkelly@gmail.com>
2020-06-01 17:40:14 -0400

    <message>

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 
- express = require("express");
+express = require("express");
 
 var PORT = process.env.PORT || 8080;
 
@@ -26,7 +26,7 @@ app.use(routes);
 // app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
 });
